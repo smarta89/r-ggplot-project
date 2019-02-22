a2b1a519ffb595d537797e1a2aee1312663992fc
Amy Smart <amy.smart@students.mq.edu.au>
2019-02-22 11:32:07 +1100

    pretty new plot

--- scripts/workshop.R
+++ scripts/workshop.R
@@ -44,7 +44,26 @@ interviews_plot <- ggplot(data = interviews_plotting,
   geom_jitter(alpha = 0.5, width =0.15, height = 0.15)
 ##to save this line of data as interviews_plot
 
-ggsave ("fig_output/membrs_item.png, interviews_plot, width = 15, height = 10, dpi = 72)
-## to save 
+ggsave ("fig_output/membrs_item.png", interviews_plot, width = 15, height = 10, dpi =72)
+## to save the plot with specific dimensions and clarity dpi 
+
+interviews_plot <-ggplot(data = interviews_plotting, 
+                          aes (x = no_membrs, y = number_items, color=village)) +
+  geom_jitter(alpha = 0.5, width =0.2, height = 0.2, size = 5)+
+  geom_smooth(method="lm")
+
+## to change point size 
+## to change colours
+## smooth to add line of fit
+## if it doesnt plot the change highlight what you want it to do and then press control enter
+
+ggsave("fig_output/membrs_itm.png", interviews_plot, width=15, height=10, dpi=72)
+##to save the new plot over the old plot with specifics
+
+
+
+
+
+
 
 
